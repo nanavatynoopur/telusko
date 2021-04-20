9d220ff1c7bb732d4a74fec53ee410560c26b912
Nanavaty Noopur <noopnanavaty1829@gmail.com>
2021-04-20 15:46:40 +0530

    first commit

---
+++ scorilo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ScoriloConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'scorilo'
---
+++ scorilo/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, "index.html")
+
+#def add(request):
+
+    #val1 = int(request.POST['num1'])
+    #val2 = int(request.POST['num2'])
+    #res = val1 + val2
+
+    #return render(request, "result.html", {'result':res})
